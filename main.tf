ad2c32de0527e87c79e0dce59346ffee64e5de9c
lip2050 <lip2050@users.noreply.github.com>
2023-08-25 03:32:54 -0400

    Terraform

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+/*
 resource "aws_iam_policy" "policy" {
   name        = "${var.component}-${var.env}-ssm-pm-policy"
   path        = "/"
@@ -116,3 +117,35 @@ resource "null_resource" "ansible" {
 }
 
 
+*/
+
+resource "aws_security_group" "sg" {
+  name        = "${var.component}-${var.env}-sg"
+  description = "${var.component}-${var.env}-sg"
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.component}-${var.env}-sg"
+  }
+}
+
+resource "aws_instance" "test" {
+  ami = data.aws_ami.ami.id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.sg.id]
+  subnet_id = var.subnet_id
+}
